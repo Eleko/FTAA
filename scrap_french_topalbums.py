9ab1b82907d9bf997b9b27a3b207d334909f881b
Eleko <valentinchabaux@gmail.com>
2021-10-03 12:43:33 +0200

    Create scrap_french_topalbums.py

--- scrap_french_topalbums.py
+++ scrap_french_topalbums.py
@@ -1,9 +1,13 @@
+#scrap Syndicat national de l'édition phonographique (SNEP) / 1994 - 2020 Top Albums
+
 from bs4 import BeautifulSoup
 import urllib.request
 import csv
 import pandas as pd
 import time
+
 topalbums = pd.DataFrame(columns=['year', 'rank', 'title', 'artist', 'editor'])
+
 for year in range(1994, 2021) :
     url = 'https://snepmusique.com/les-tops/le-top-de-lannee/top-albums-annee/?annee={}'.format(year)
     page = urllib.request.urlopen(url)
@@ -11,17 +15,17 @@ for year in range(1994, 2021) :
     items = soup.find('div', attrs={'class': 'items'})
     results = items.find_all('div', attrs={'class': 'item'})
     last_rank = None
+    
     for album in results:
         title = album.find('div', attrs={'class': 'titre'}).string
         artist = album.find('div', attrs={'class': 'artiste'}).string
         editor_temp = album.find('div', attrs={'class': 'editeur'}).string
-        rank = album.find('div', attrs={'class': 'rang'}).string
-        
+        rank = album.find('div', attrs={'class': 'rang'}).string        
         if editor_temp != None:
             editor = editor_temp.rstrip()
         else :
             editor = None
-        
+            
         row = {
             'year' : year,
             'rank' : rank,
@@ -29,9 +33,12 @@ for year in range(1994, 2021) :
             'artist' : artist.rstrip(),
             'editor' : editor
         }
+        
         topalbums = topalbums.append(row, ignore_index=True)
         last_rank = rank
+        
     print('Scraping top {} albums for year {}'.format(last_rank, year))    
     time.sleep(1)
+    
 print('Scraping finished')
 top50.to_csv('top-albums_1994-2020.csv', index=False)
